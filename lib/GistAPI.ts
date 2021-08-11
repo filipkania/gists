9a5445cadda905bd45cc560defda38c58ee1b47a
Filip Kania <fkania2@gmail.com>
2021-08-11 20:32:10 +0200

    Libs: added GistAPI wrapper

--- lib/GistAPI.ts
+++ lib/GistAPI.ts
@@ -1,1 +1,23 @@
-export default class {}
+import axios, { AxiosInstance } from "axios";
+class GistAPI {
+	private token: string;
+	public readonly client: AxiosInstance;
+
+	constructor(token: string) {
+		this.token = token;
+		this.client = axios.create({
+			url: "https://api.github.com",
+			responseType: "json",
+			headers: {
+				Accept: "application/json",
+				Authorization: `token ${this.token}`,
+			},
+		});
+	}
+
+	public validateToken = (token: string) => {
+		return !!token;
+	};
+}
+
+export default GistAPI;
